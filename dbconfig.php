bc435e3c5cd7cf3df4d41eea77a3bd13ad6f43b8
HeatBoxProject <37890425+HeatBoxProject@users.noreply.github.com>
2018-03-29 04:38:33 +0100

    Create dbconfig.php

---
+++ dbconfig.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_USER', "id5154569_th");     // Your database user name
+define('DB_PASSWORD', "jhonhenry");			// Your database password (mention your db password here)
+define('DB_DATABASE', "id5154569_temphumid"); // Your database name
+define('DB_SERVER', "localhost");			// db server (Mostly will be 'local' host)
+
+?>
